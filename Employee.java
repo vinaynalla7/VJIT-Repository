f36ee33b14bc6f3bd2f848b82f1254410a79d209
vinaynalla7 <60592963+vinaynalla7@users.noreply.github.com>
2020-02-11 14:39:04 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,25 @@
+package jenkins;
+
+public class Employee {
+    String Empname;
+    int Empid;
+    float sal;
+    Employee(){
+    }
+    Employee(String Empname,int Empid,float sal){
+    this.Empname=Empname;
+    this.Empid=Empid;
+    this.sal=sal;
+    }
+    public void calculateSalary(){
+    System.out.println("Employee salary:"+(sal+2000));
+    }
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+     Employee e=new Employee();
+     e.calculateSalary();
+}
+
+}
+
+
